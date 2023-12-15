7071cd42ad69e8cc6b4d740fb7f9d4ebf06c755f
shinyay <shinya.com@gmail.com>
2023-12-15 22:14:03 +0900

    Modify package name

--- introducing-python/chpater11/chapter11.1/month.py
+++
@@ -1,7 +0,0 @@
-from random import choice
-
-months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
-
-def choose_month():
-    """Return the raondom month"""
-    return choice(months)
